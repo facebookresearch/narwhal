6a1da45185316cc57fa71078f801e33bbf620622
Alberto Sonnino <asonnino@fb.com>
2021-06-23 12:12:03 +0100

    Remove old files

--- rust/primary/src/error.rs
+++ rust/primary/src/error.rs
@@ -1,5 +1,4 @@
 use crypto::{CryptoError, Digest, PublicKey};
-use rocksdb;
 use store::StoreError;
 use thiserror::Error;
 
@@ -19,27 +18,8 @@ macro_rules! ensure {
     };
 }
 
-// OLD macros
-
-#[macro_export]
-macro_rules! dag_bail {
-    ($e:expr) => {
-        return Err($e);
-    };
-}
-
-#[macro_export(local_inner_macros)]
-macro_rules! dag_ensure {
-    ($cond:expr, $e:expr) => {
-        if !($cond) {
-            dag_bail!($e);
-        }
-    };
-}
-
 pub type DagResult<T> = Result<T, DagError>;
 
-/// Custom error type.
 #[derive(Debug, Error)]
 pub enum DagError {
     #[error("Invalid signature")]
@@ -71,81 +51,4 @@ pub enum DagError {
 
     #[error("Parents of header {0} are not a quorum")]
     HeaderRequiresQuorum(Digest),
-
-    // OLD
-    #[error("The digest provided is different from the digest computed.DagError")]
-    BadDigest,
-
-    #[error("This block has not been delivered to consensus")]
-    NoBlockAvailable,
-
-    #[error("Value was not signed by a known authority")]
-    UnknownSigner,
-
-    #[error("Signatures in a certificate must be from different authorities")]
-    CertificateAuthorityReuse,
-
-    #[error("We received a partial certificate from ourselves?")]
-    CertificateAuthorityOurselves,
-
-    // Signatures in a certificate must form a quorum
-    //CertificateRequiresQuorum,
-
-    // Block integrity checks.
-    #[error("Block must contain 2f+1 references to parents blocks")]
-    BadNumberOfParents,
-
-    // Internal Serialization
-    // Header or certificate internal serialization failed
-    //SerializationError(bincode::ErrorKind),
-    #[error("Wrong instance Id")]
-    BadInstanceId,
-
-    #[error("We received a late or unexpected partial certificate")]
-    UnexpectedOrLatePartialCertificate,
-
-    // Storage
-    #[error("Storage error: {error}")]
-    StorageFailure { error: String },
-
-    // Channels
-    #[error("Channel error: {error}")]
-    ChannelError { error: String },
-
-    // Network
-    #[error("Network error: {error}")]
-    NetworkError { error: String },
-
-    #[error("Unknown node name")]
-    UnknownNode,
-
-    #[error("Skip Message Round")]
-    ChannelClosed,
-
-    #[error("Received conflicting blockheader")]
-    ConflictingHeader,
-
-    #[error("parents are not from previous round or has not enough stake")]
-    WrongParents,
-
-    #[error("Message Handle is dropped and message will not be sent")]
-    MessageHandleDropped,
-
-    // Something is seriously wrong with our internal logic
-    #[error("Internal error: {error}")]
-    InternalError { error: String },
-
-    #[error("The blockheader is not consistent with the provided certificate")]
-    BadHeaderDigest,
-
-    // Invalid signature
-    //InvalidSignature(#[from] CryptoError),
-    #[error("Store error")]
-    RocksdbError(rocksdb::Error),
-}
-
-impl From<rocksdb::Error> for DagError {
-    fn from(e: rocksdb::Error) -> Self {
-        DagError::RocksdbError(e)
-    }
 }
--- rust/primary/src/lib.rs
+++ rust/primary/src/lib.rs
@@ -1,11 +1,3 @@
-#![recursion_limit = "1024"]
-
-#[macro_use]
-extern crate failure;
-extern crate base64;
-extern crate ed25519_dalek;
-extern crate rstest;
-
 #[macro_use]
 mod error;
 mod aggregators;
@@ -18,17 +10,6 @@ mod payload_receiver;
 mod primary;
 mod proposer;
 mod synchronizer;
-
-pub mod committee;
-pub mod manage_primary;
-pub mod old_messages;
-pub mod old_primary;
-pub mod old_synchronizer;
-pub mod primary_net;
-pub mod processor;
-mod sync_driver;
-pub mod sync_server;
-pub mod types;
 
 pub use crate::messages::{Certificate, Header};
 pub use crate::primary::{Primary, PrimaryWorkerMessage, Round, WorkerPrimaryMessage};
