575da22d8a9cce9276a27423f51eb6f1c736e15a
Alberto Sonnino <asonnino@fb.com>
2021-08-06 13:46:28 +0100

    Tweak control sys

--- hotstuff/src/proposer.rs
+++ hotstuff/src/proposer.rs
@@ -9,6 +9,7 @@ use log::{debug, info};
 use network::{CancelHandler, ReliableSender};
 use primary::Certificate;
 use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::time::{sleep, Duration};
 
 #[derive(Debug)]
 pub struct ProposerMessage(pub Round, pub QC, pub Option<TC>);
@@ -128,6 +129,9 @@ impl Proposer {
                 },
                 Some(ProposerMessage(round, qc, tc)) = self.rx_message.recv() =>  {
                     self.make_block(round, qc, tc).await;
+
+                    // TODO: Ugly control system.
+                    sleep(Duration::from_millis(100)).await;
                 }
             }
         }
