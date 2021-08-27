7b53b58be0e67cee9f79f37b64118010689c7de7
Alberto Sonnino <asonnino@fb.com>
2021-08-27 20:09:18 +0100

    Fix bug in cleanup virtual dag

--- consensus/src/dolphin/virtual_state.rs
+++ consensus/src/dolphin/virtual_state.rs
@@ -77,11 +77,10 @@ impl VirtualState {
     /// Cleanup the internal state after committing a certificate.
     pub fn cleanup(&mut self, last_committed_round: Round, gc_depth: Round) {
         self.dag.retain(|r, _| r + gc_depth > last_committed_round);
-
         self.steady_authorities_sets
-            .retain(|w, _| w > &((last_committed_round + 1) / 2));
+            .retain(|w, _| w >= &((last_committed_round + 1) / 2));
         self.fallback_authorities_sets
-            .retain(|w, _| w > &((last_committed_round + 1) / 4));
+            .retain(|w, _| w >= &((last_committed_round + 1) / 4));
     }
 
     /// Returns the certificate (and the certificate's digest) originated by the steady-state leader
