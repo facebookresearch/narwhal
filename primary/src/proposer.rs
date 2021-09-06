6fa21a8d393dea9c1f79273d1a24b64b9320a045
Alberto Sonnino <asonnino@fb.com>
2021-09-06 16:26:24 +0100

    debug

--- primary/src/proposer.rs
+++ primary/src/proposer.rs
@@ -149,7 +149,7 @@ impl Proposer {
             let timer_expired = timer.is_elapsed();
             //let certified = self.certified.load(Ordering::Relaxed);
             let advance_early = !self.metadata.is_empty();
-            if (timer_expired || enough_digests || advance_early) && enough_parents {
+            if (timer_expired || enough_digests) && enough_parents {
                 // Make a new header.
                 self.make_header().await;
                 self.payload_size = 0;
